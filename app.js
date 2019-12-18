13287388e02b840e620b30fd2e92f96fa4882aed
Mark Leo <ratrateroo@gmail.com>
2019-12-19 03:14:46 +0800

    tested request and response

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
-
 const userName = 'Mark';
 
-fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('WROTE FILE');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('INCOMING REQUEST');
+  console.log(req.method, req.url);
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Success!</h1>');
 });
+
+server.listen(5000);
